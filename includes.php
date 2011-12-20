9fde3ecb3fac30df04c1861e79b46c8260309625
Michael Robinson <mike@pagesofinterest.net>
2011-12-20 22:55:37 +1300

    Moved includes into require, added XMod check to includes.

--- includes.php
+++ includes.php
@@ -1,6 +1,9 @@
 <?php
 namespace COI\Social;
 
-include('COI/Social/Elements/Base.php');
-include('COI/Social/Elements/Facebook.php');
-include('COI/Social/Manager.php');
+use XMod\Common\AutoLoader;
+if (class_exists('XMod\Common\AutoLoader')) {
+    AutoLoader::register('XMod', __DIR__.'/classes');
+} else {
+    require_once __DIR__.'/require.php';
+} 
---
+++ requires.php
@@ -0,0 +1,6 @@
+<?php
+namespace COI\Social;
+
+include('COI/Social/Elements/AbstractElement.php');
+include('COI/Social/Elements/Facebook.php');
+include('COI/Social/Manager.php');
