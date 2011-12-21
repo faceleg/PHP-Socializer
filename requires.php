c49a48bd3ed0f916d33ec8083adff9132f9f19e4
Michael Robinson <mike@pagesofinterest.net>
2011-12-22 00:46:08 +1300

    Added Google Plus to requires.

--- requires.php
+++ requires.php
@@ -5,4 +5,5 @@ include('COI/Social/AbstractElement.php');
 include('COI/Social/Facebook.php');
 include('COI/Social/Twitter.php');
 include('COI/Social/LinkedIn.php');
+include('COI/Social/GooglePlus.php');
 include('COI/Social/Manager.php');
