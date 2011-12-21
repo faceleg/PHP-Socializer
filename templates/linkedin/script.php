0aa901aae71da0377b272f77a16b2b8bf6d64a18
Michael Robinson <mike@pagesofinterest.net>
2011-12-21 22:54:55 +1300

    Added LinkedIn class & templates.

---
+++ templates/linkedin/script.php
@@ -0,0 +1,20 @@
+<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="text/javascript">    
+    // COI\Social - Linked In
+    (function(){
+        
+
+         _ga.trackLinkedIn = {
+            success: function() {
+/*
+                 _gaq.push([trackerName + '_trackSocial', 'linkein', 'share',
+                    targetUrl, opt_pageUrl]);
+*/
+            },
+            error: function() {
+                alert(arguments);
+            }
+         };
+
+    })();
+</script>
