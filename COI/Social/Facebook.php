49404be481fbbacbe2612e77a578f006f67d3623
Michael Robinson <mike@pagesofinterest.net>
2011-12-21 23:46:22 +1300

    Updated Facebook button's default width.

--- COI/Social/Facebook.php
+++ COI/Social/Facebook.php
@@ -17,7 +17,7 @@ class Facebook extends AbstractElement {
     public function __construct($options = array()) {
         $appId = null;
         $url = getCurrentUrl();
-        $width = 200;
+        $width = 150;
         $showFaces = 'false';
         $sendButton = 'true';
         $channelUrl = '//'.$_SERVER['HTTP_HOST'].'/facebook-channel.php';
