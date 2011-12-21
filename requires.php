2f9b0bf12a1e8adfc8cdeb2f60fa54dd9e7d761f
Michael Robinson <mike@pagesofinterest.net>
2011-12-21 22:55:40 +1300

    Updated requres.

--- requires.php
+++ requires.php
@@ -3,4 +3,6 @@ namespace COI\Social;
 
 include('COI/Social/Elements/AbstractElement.php');
 include('COI/Social/Elements/Facebook.php');
+include('COI/Social/Elements/Twitter.php');
+include('COI/Social/Elements/LinkedIn.php');
 include('COI/Social/Manager.php');
