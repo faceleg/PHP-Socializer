85623939d0e281d74203b5c3a3bac0f051655368
Michael Robinson <mike@pagesofinterest.net>
2011-12-22 22:49:44 +1300

    Lame workaround for LinkedIn's broken in share callbacks...

--- templates/linkedin/script.php
+++ templates/linkedin/script.php
@@ -4,14 +4,19 @@
     (function(){
         var trackerName = _ga.buildTrackerName_();
         _ga.trackLinkedIn = {
+            // LinkedIn callbacks seem broken, I've included these anyway...
             success: function() {
-                console.log(arguments);
                 _gaq.push([trackerName + '_trackSocial', 'linkedin', 'share', targetUrl, opt_pageUrl]);
             },
             error: function() {
                 _gaq.push([trackerName + '_trackSocial', 'linkedin', 'error', targetUrl, opt_pageUrl]);
             }
          };
-
+         // If jQuery is present, bind a click event to the linked in anchor and let Google know about any clicks we capture
+         if (jQuery) {
+            $('.IN-widget').click(function(){
+                _gaq.push([trackerName + '_trackSocial', 'linkedin', 'click', $(this).parents('div.coi-social-button-linkedIn').find('script').data('url')]);
+            });
+        }
     })();
 </script>
