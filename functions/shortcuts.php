deb870beff58762a248626c8ab3488a293ceefd3
Michael Robinson <mike@pagesofinterest.net>
2012-03-02 00:29:36 +1300

    Continued refactoring

---
+++ functions/shortcuts.php
@@ -0,0 +1,30 @@
+<?php
+namespace COI\Social;
+
+function Facebook(array $options = array()) {
+    return new Facebook($options);
+}
+
+function Flattr(array $options = array()) {
+    return new Flattr($options);
+}
+
+function GitHub(array $options = array()) {
+    return new GitHub($options);
+}
+
+function GooglePlus(array $options = array()) {
+    return new GooglePlus($options);
+}
+
+function LinkedIn(array $options = array()) {
+    return new LinkedIn($options);
+}
+
+function StumbleUpon(array $options = array()) {
+    return new StumbleUpon($options);
+}
+
+function Twitter(array $options = array()) {
+    return new Twitter($options);
+}
