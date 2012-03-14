cc0f5b2d09a10b5a8a81cdb3221a9d689f30fd31
Michael Robinson <mike@pagesofinterest.net>
2012-03-14 23:24:17 +1300

    Updated twitter class to allow types

--- classes/COI/Social/Twitter.php
+++ classes/COI/Social/Twitter.php
@@ -5,6 +5,10 @@ use COI\Social\Twitter\Counter;
 define('COI\Social\Twitter\Counter\HORIZONTAL', 'horizontal');
 define('COI\Social\Twitter\Counter\VERTICAL', 'vertical');
 define('COI\Social\Twitter\Counter\NONE', 'none');
+define('COI\Social\Twitter\Counter\SHOW', 'true');
+
+define('COI\Social\Twitter\Type\TWEET', 'html-tweet');
+define('COI\Social\Twitter\Type\FOLLOW', 'html-follow');
 
 class Twitter extends AbstractElement {
     
@@ -15,5 +19,9 @@ class Twitter extends AbstractElement {
     public $hashtag = null;
     public $size = null;
     public $counter = Counter\HORIZONTAL;
+    public $type = Type\TWEET;
 
+    public function getView() {
+        return $this->type;
+    }
 }
