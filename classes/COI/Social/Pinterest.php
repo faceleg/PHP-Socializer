fc3b017f9e850cf04e41f27e63e7d6cf30f34941
Michael Robinson <mike@panmedia.co.nz>
2012-03-16 13:25:34 +1300

    Added Pinterest.

---
+++ classes/COI/Social/Pinterest.php
@@ -0,0 +1,37 @@
+<?php
+namespace COI\Social;
+use COI\Social\Pinterest\Count;
+
+define('COI\Social\Pinterest\Count\VERTICAL', 'vertical');
+define('COI\Social\Pinterest\Count\HORIZONTAL', 'horizontal');
+define('COI\Social\Pinterest\Count\NONE', 'none');
+
+class Pinterest extends AbstractElement {
+
+    // Required
+
+    /**
+     * @var string URL for the object to be pinned.
+     */
+    public $url = null;
+
+    /**
+     * @var string URL for this object's site.
+     */
+    public $siteUrl = null;
+
+    /**
+     * @var string| Whether to show the count, and if so where.
+     */
+    public $layout = Count\HORIZONTAL;
+
+    // Optional
+
+    /**
+     * While a description is optional, it is recommended; specifying it lowers the
+     * friction for your users to pin your products.
+     * @var string lowers the friction for your users to pin your products.
+     */
+    public $description = null;
+
+}
