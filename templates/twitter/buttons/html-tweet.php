13d69b1f9c829ff015e834deefc14120c103fb43
Michael Robinson <mike@panmedia.co.nz>
2012-03-30 12:47:29 +1300

    Whitespace

--- templates/twitter/buttons/html-tweet.php
+++ templates/twitter/buttons/html-tweet.php
@@ -1,16 +1,16 @@
-<a href="https://twitter.com/share" 
-    class="twitter-share-button" 
-    data-url="<?php echo $url; ?>" 
-    data-text="<?php echo $title; ?>" 
-    data-via="<?php echo $username; ?>" 
+<a href="https://twitter.com/share"
+    class="twitter-share-button"
+    data-url="<?php echo $url; ?>"
+    data-text="<?php echo $title; ?>"
+    data-via="<?php echo $username; ?>"
     data-count="<?php echo $counter; ?>"
     <?php if($recommend): ?>
     data-related="<?php echo $recommend; ?>"
     <?php endif; ?>
     <?php if($hashtag): ?>
     data-hashtags="<?php echo $hashtag; ?>"
-    <?php endif; ?> 
+    <?php endif; ?>
     <?php if($size): ?>
     data-hashtags="<?php echo $size; ?>"
-    <?php endif; ?> 
+    <?php endif; ?>
     data-lang="en">Tweet</a>
