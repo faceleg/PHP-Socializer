e59fdb371b0a73b461b1a55393df5417b088c37a
Michael Robinson <mike@panmedia.co.nz>
2012-04-05 11:35:24 +1200

    Removed php script templates

--- templates/github/script.php
+++
@@ -1,9 +0,0 @@
-<?php if ($this->fadeIn): ?>
-<script type="text/javascript">
-    // COI\Social - GitHub
-    (function() {
-        var buttons = document.getElementsByClassName('coi-social-button-<?php echo $this->name; ?>');
-        fadeIn(buttons, <?php echo $this->fadeIn; ?>);
-    })();
-</script>
-<?php endif; ?>
