7439a34f2cb755e27d4a75cacc5b024c0aaa131d
Michael Robinson <mike@pagesofinterest.net>
2012-11-08 03:37:07 -0700

    Added the annotation value to GooglePlus templatey

--- templates/googleplus/buttons/html.php
+++ templates/googleplus/buttons/html.php
@@ -1,4 +1,4 @@
-<div class="g-plusone" callback="_ga.googlePlus" 
+<div class="g-plusone" callback="_ga.googlePlus"
     data-href="<?php echo $url; ?>"
     data-size="<?php echo $size; ?>"
-    data-annotation=""></div>
+    data-annotation="<?php echo $annotation; ?>"></div>
