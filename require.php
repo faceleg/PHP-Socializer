f186f2e32c5f2051449da6a71f1d8e5bee2378ac
Michael Robinson <mike@panmedia.co.nz>
2012-12-18 09:29:12 +1300

    Updated require

--- require.php
+++ require.php
@@ -1,16 +1,21 @@
 <?php
 namespace COI\Social;
 
-include(__DIR__.'/classes/COI/Social/AbstractElement.php');
-include(__DIR__.'/classes/COI/Social/Facebook.php');
-include(__DIR__.'/classes/COI/Social/Twitter.php');
-include(__DIR__.'/classes/COI/Social/LinkedIn.php');
-include(__DIR__.'/classes/COI/Social/GooglePlus.php');
-include(__DIR__.'/classes/COI/Social/Flattr.php');
-include(__DIR__.'/classes/COI/Social/StumbleUpon.php');
-include(__DIR__.'/classes/COI/Social/GitHub.php');
-include(__DIR__.'/classes/COI/Social/Pinterest.php');
+include_once __DIR__.'/classes/COI/Social/AbstractElement.php';
+include_once __DIR__.'/classes/COI/Social/Facebook.php';
+include_once __DIR__.'/classes/COI/Social/Twitter.php';
+include_once __DIR__.'/classes/COI/Social/LinkedIn.php';
+include_once __DIR__.'/classes/COI/Social/GooglePlus.php';
+include_once __DIR__.'/classes/COI/Social/Flattr.php';
+include_once __DIR__.'/classes/COI/Social/StumbleUpon.php';
+include_once __DIR__.'/classes/COI/Social/GitHub.php';
+include_once __DIR__.'/classes/COI/Social/Pinterest.php';
 
-include(__DIR__.'/classes/COI/Social/Manager.php');
-include(__DIR__.'/classes/COI/Social/Compressor.php');
-include(__DIR__.'/classes/COI/Social/Cache.php');
+include_once __DIR__.'/classes/COI/Social/Manager.php';
+include_once __DIR__.'/classes/COI/Social/Compressor.php';
+include_once __DIR__.'/classes/COI/Social/Cache.php';
+
+include_once __DIR__.'/functions/local.php';
+include_once __DIR__.'/functions/shortcuts.php';
+
+include_once __DIR__.'/constants.php';
