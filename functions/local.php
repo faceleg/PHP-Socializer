a05e7fa3d9103ef9592d68609c744fed9d677e9c
Michael Robinson <mike@panmedia.co.nz>
2012-12-18 09:15:26 +1300

    Whitespace

--- functions/local.php
+++ functions/local.php
@@ -7,20 +7,20 @@ function getClassName($object) {
     }
 
     return trim(strrchr((is_string($object) ? $object : get_class($object)), '\\'), '\\');
-} 
+}
 
 function getCurrentUrl() {
 	if (!isset($_SERVER)) {
 		return '';
 	}
     $pageURL = 'http';
-  	
+
     if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
         $pageURL .= 's';
     }
-    
+
     $pageURL .= '://';
-    
+
     if ($_SERVER['SERVER_PORT'] != '80') {
         $pageURL .= $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
     } else {
