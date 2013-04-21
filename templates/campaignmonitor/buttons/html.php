2848641b33b73918576531beb4306724f4b14ad4
Michael Robinson <mike@panmedia.co.nz>
2013-04-22 11:39:32 +1200

    Removed inline styles

--- templates/campaignmonitor/buttons/html.php
+++ templates/campaignmonitor/buttons/html.php
@@ -1,1 +1,1 @@
-<div class="createsend-button" style="height:27px;display:inline-block;" data-listid="<? echo $this->listId; ?>"></div>
+<div class="createsend-button" data-listid="<? echo $this->listId; ?>"></div>
